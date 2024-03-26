a0d9fcce0be3be7a3d533813d8cb1fadfefc43ef
Juliana <julianafulanetto.senai@gmail.com>
2024-03-26 10:16:58 -0300

    Alterações do dia 26

--- Aula02/js/script.js
+++ Aula02/js/script.js
@@ -1,3 +1,17 @@
 alerta = () => {
-    alert("Olá, eu sou um alerta!🚨")
-}
+  alert("Olá, eu sou um alerta!🚨");
+};
+
+const enviar = () => {
+  let nome = document.getElementById("nome").value;
+
+  document.getElementById(
+    "resposta"
+  ).innerHTML = `<h2>Olá ${nome}, formulário eviado com sucesso!</h2>`;
+
+  if (nome == "") {
+    alert("Bem vindo, digite seus dados!");
+  } else {
+    alert(`Olá ${nome}, bem vindo!`);
+  }
+};
